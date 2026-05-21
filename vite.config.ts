1f65d6b1f0882f7bfc2425112e7f419e3f1ab34a
raka1 <p.raka54@ymail.com>
2026-05-22 05:51:28 +0700

    feat: setup arsitektur bersih tanpa reverse proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,35 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  process.env = loadEnv(mode, process.cwd())
+
+  const apiProxyConfig = {
+    target: process.env.VITE_API_URL,
+    changeOrigin: true,
+    secure: false,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
     },
-  },
+    rewrite: (path: string) => path.replace(/^\/api\/v1/, ''),
+  }
+
+  return {
+    plugins: [vue(), vueJsx(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    server: {
+      proxy: {
+        '/api/v1': apiProxyConfig,
+      },
+    },
+  }
 })
