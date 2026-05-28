0622572380f54b897e2ffa594ac40ce0a4a56efd
raka1 <p.raka54@ymail.com>
2026-05-28 19:46:21 +0700

    Search page minor performance fix

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,35 +1,11 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue'
 import ProductFilter from '@/components/product/ProductFilter.vue'
 import Products from '@/components/product/ProductList.vue'
-
-const filterRef = ref<HTMLDivElement | null>(null)
-
-function resizeWatcher() {
-  const width = window.innerWidth
-
-  if (filterRef.value) {
-    if (width < 768) {
-      filterRef.value.style.display = 'none'
-    } else {
-      filterRef.value.style.display = ''
-    }
-  }
-}
-
-onMounted(() => {
-  window.addEventListener('resize', resizeWatcher)
-  resizeWatcher()
-})
-
-onUnmounted(() => {
-  window.removeEventListener('resize', resizeWatcher)
-})
 </script>
 
 <template>
   <div class="row mt-4">
-    <div class="col-md-4 col-lg-3 col-xl-2" ref="filterRef">
+    <div class="col-md-4 col-lg-3 col-xl-2 sm-hide">
       <ProductFilter />
     </div>
     <div class="col-12 col-md-8 col-lg-9 col-xl-10">
